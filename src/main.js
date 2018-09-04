750eae773e48ba58a3b225f05216f87119a777b0
Ivan <ivantran2290@gmail.com>
2018-09-05 01:09:10 +0900

    add master

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+
+Vue.config.productionTip = false
+Vue.use(Vuetify)
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
